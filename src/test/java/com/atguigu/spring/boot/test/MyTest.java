9a2dc3e88647e753f57e0cbe45ba157be889cf4f
harry <harry@atguigu.com>
2020-06-26 19:27:26 +0800

    Initial commit

---
+++ src/test/java/com/atguigu/spring/boot/test/MyTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.spring.boot.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MyTest {
+
+    @Test
+    public void test01() {
+        System.out.println("hello spring test");
+    }
+
+}
